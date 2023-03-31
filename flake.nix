b5a3187850ed424726806142f0bae0c9592704bc
Sam Stites <stites@users.noreply.github.com>
2023-03-31 11:22:16 -0400

    pull in menu function

--- flake.nix
+++ flake.nix
@@ -1,47 +1,49 @@
 {
-  # inputs = {
-  # };
-
   outputs = {
     self,
     nixpkgs,
     ...
   } @ inputs: let
+    # systems I like
     systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-    nixlib = pkgs:
-      with builtins; rec {
-        forAllSystems = f:
-          listToAttrs (map (name: {
-              inherit name;
-              value = f name;
-            })
-            systems);
-        maxStringLength = names:
-          with builtins;
-            foldl' (mx: p:
-              if stringLength name > mx
-              then stringLength name
-              else mx)
-            0
-            names;
-        packageNames = ps:
-          map (p:
-            if hasAttr "pname" p
-            then p.pname
-            else p.name)
-          ps;
-        descriptions = ps:
-          map (p:
-            if hasAttr "meta" p
-            then p.meta.description
-            else "${p}")
-          ps;
-        menu = packages: with pkgs.lib.strings; with pkgs.lib.lists; let
+    lib = with builtins; rec {
+      forAllSystems = f:
+        listToAttrs (map (name: {
+            inherit name;
+            value = f name;
+          })
+          systems);
+      maxStringLength = names:
+        with builtins;
+          foldl' (mx: name:
+            if stringLength name > mx
+            then stringLength name
+            else mx)
+          0
+          names;
+      packageNames = ps:
+        map (p:
+          if hasAttr "pname" p
+          then p.pname
+          else p.name)
+        ps;
+      descriptions = ps:
+        map (p:
+          if hasAttr "meta" p && hasAttr "description" p.meta
+          then p.meta.description
+          else "${p}")
+        ps;
+      menu = {
+        pkgs,
+        packages,
+      }:
+        with pkgs.lib.strings;
+        with pkgs.lib.lists; let
           mx = maxStringLength names;
           names = packageNames packages;
           descs = descriptions packages;
           rpads = map (name: let
-            padSize = n - (stringLength name);
+            padSize = mx - (stringLength name);
           in
             pkgs.lib.strings.fixedWidthString padSize " " "");
         in
@@ -59,18 +61,17 @@
                 rightpad = tpl.fst.snd;
                 description = tpl.snd;
               in "echo \"${name}${rightpad}\t-- ${description}\""
-            ) (zipLists (zipLists names rpads) descs))
+            ) (zipLists (zipLists names (rpads names)) descs))
           );
-      };
+    };
   in {
-    lib = nixlib;
-    devShells = let
-      system = "x86_64-linux";
+    inherit lib;
+    devShells = lib.forAllSystems (system: let
       pkgs = import nixpkgs {inherit system;};
     in {
-      ${system}.default = pkgs.mkShell {
+      default = pkgs.mkShell {
         buildInputs = [pkgs.alejandra];
       };
-    };
+    });
   };
 }
